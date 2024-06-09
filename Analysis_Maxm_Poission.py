ad1a57214f1d6e552c58e98eec151b967fa406a1
Kritan Shrestha <161418388+Whitoan-2@users.noreply.github.com>
2024-06-09 14:54:28 +0545

    Analysis_Maxm_Poission.py

--- Analysis_Maxm_Poission.py
+++ Analysis_Maxm_Poission.py
@@ -2,15 +2,17 @@ import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 start = 1
-end = 10
+end = 120
 m = np.linspace(start,end,end-start+1)
 val = []
 for e in m:
-    x = np.linspace(e - 1, e, 100)
-    y = np.exp(e) * np.exp(np.log(e) * x) / sp.special.gamma(x + 1)
+    x = np.linspace(e - 1, e, 100000)
+    y = np.exp(-e) * e ** x / sp.special.gamma(x + 1)
     y2 = np.gradient(y, x)
     xt = x[1:]
     xint = xt[y2[1:] * y2[:-1] < 0]
     val.append(e-xint-0.5)
+plt.xlabel("λ")
+plt.ylabel("λ - X_Max - 0.5")
 plt.plot(m,val)
 plt.show()
